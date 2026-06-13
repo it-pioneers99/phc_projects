4182c9e29cab401f6ff049d8dcbe2d0b1667fe9c
it-pioneers99 <it@pioneersholding.ae>
2026-06-13 17:29:55 +0300

    allow submit item type 8898435

--- phc_projects/phc_projects/budget_control/purchase_order_validation.py
+++ phc_projects/phc_projects/budget_control/purchase_order_validation.py
@@ -58,8 +58,63 @@ def get_effective_budget_for_item(project, budget_item):
 
 
 def get_committed_budget_usage(used_po, used_pc):
-	"""Committed budget is PO + PC only; PI is tracked separately (see Budget Expense)."""
+	"""Difference = Budget - (PO + PC). PI is tracked separately, not against budget."""
 	return flt(used_po) + flt(used_pc)
+
+
+def get_available_budget(total_budget, used_po, used_pc):
+	committed = get_committed_budget_usage(used_po, used_pc)
+	return flt(total_budget) - committed, committed
+
+
+def throw_budget_exceeded(budget_item, total_budget, used_po, used_pi, used_pc, doc_amount, doc_label):
+	available_budget, committed = get_available_budget(total_budget, used_po, used_pc)
+	exceeded_amount = (committed + doc_amount) - flt(total_budget)
+	frappe.throw(
+		_("Budget exceeded for Budget Expense Item: <b>{0}</b><br><br>"
+		  "Total Budget: {1}<br>"
+		  "Used Cost (PO): {2}<br>"
+		  "Used Cost (PC): {3}<br>"
+		  "Difference (Budget - PO - PC): {4}<br>"
+		  "Used Cost (PI): {5}<br>"
+		  "This {6} Amount: {7}<br>"
+		  "<b>Exceeded by: {8}</b>").format(
+			budget_item,
+			frappe.format(total_budget, {"fieldtype": "Currency"}),
+			frappe.format(used_po, {"fieldtype": "Currency"}),
+			frappe.format(used_pc, {"fieldtype": "Currency"}),
+			frappe.format(available_budget, {"fieldtype": "Currency"}),
+			frappe.format(used_pi, {"fieldtype": "Currency"}),
+			doc_label,
+			frappe.format(doc_amount, {"fieldtype": "Currency"}),
+			frappe.format(exceeded_amount, {"fieldtype": "Currency"}),
+		),
+		title=_("Budget Exceeded"),
+	)
+
+
+def check_budget_for_item(project, budget_item, amount, doc_label):
+	"""Validate item budget: Difference = Budget - (PO + PC). PI is informational only."""
+	budget_info = get_effective_budget_for_item(project, budget_item)
+
+	if not budget_info:
+		frappe.throw(
+			_("No budget found for Budget Expense Item: {0} in Project: {1}. Please create a Budget Expense document first.").format(
+				budget_item, project
+			),
+			title=_("Budget Not Found"),
+		)
+
+	total_budget = flt(budget_info["total_budget"])
+	used_po = flt(budget_info["used_po"])
+	used_pi = flt(budget_info["used_pi"])
+	used_pc = flt(budget_info["used_pc"])
+	_, committed = get_available_budget(total_budget, used_po, used_pc)
+
+	if committed + amount > total_budget:
+		throw_budget_exceeded(
+			budget_item, total_budget, used_po, used_pi, used_pc, amount, doc_label
+		)
 
 
 def validate_budget_on_po_submit(doc, method):
@@ -90,66 +145,7 @@ def validate_budget_on_po_submit(doc, method):
 	
 	# Validate each budget item
 	for budget_item, po_amount in budget_map.items():
-		# Get budget from submitted Budget Expense documents
-		budget_data = frappe.db.sql("""
-			SELECT
-				bed.budget_item_name,
-				SUM(bed.budget_item_cost) as total_budget,
-				SUM(bed.used_cost_po) as used_po,
-				SUM(bed.used_cost_pi) as used_pi,
-				SUM(bed.used_cost_pc) as used_pc,
-				be.project
-			FROM `tabBudget Expense Detail` bed
-			INNER JOIN `tabBudget Expense` be ON bed.parent = be.name
-			WHERE bed.budget_item_name = %s
-			AND be.project = %s
-			AND be.docstatus = 1
-			GROUP BY bed.budget_item_name, be.project
-		""", (budget_item, doc.project), as_dict=True)
-		
-		if not budget_data:
-			frappe.throw(
-				_("No budget found for Budget Expense Item: {0} in Project: {1}. Please create a Budget Expense document first.").format(
-					budget_item, doc.project
-				),
-				title=_("Budget Not Found")
-			)
-		
-		budget_info = budget_data[0]
-		total_budget = flt(budget_info.total_budget or 0)
-		used_po = flt(budget_info.used_po or 0)
-		used_pi = flt(budget_info.used_pi or 0)
-		used_pc = flt(budget_info.used_pc or 0)
-		
-		# Budget commitment is PO + PC only; PI is informational (same as Budget Expense)
-		total_committed = get_committed_budget_usage(used_po, used_pc)
-		available_budget = total_budget - total_committed
-
-		# Check if this PO would exceed budget
-		if total_committed + po_amount > total_budget:
-			exceeded_amount = (total_committed + po_amount) - total_budget
-			frappe.throw(
-				_("Budget exceeded for Budget Expense Item: <b>{0}</b><br><br>"
-				  "Total Budget: {1}<br>"
-				  "Already Used (PO): {2}<br>"
-				  "Already Used (PI): {3}<br>"
-				  "Already Used (PC): {4}<br>"
-				  "Committed (PO + PC): {5}<br>"
-				  "This PO Amount: {6}<br>"
-				  "Available Budget: {7}<br>"
-				  "<b>Exceeded by: {8}</b>").format(
-					budget_item,
-					frappe.format(total_budget, {"fieldtype": "Currency"}),
-					frappe.format(used_po, {"fieldtype": "Currency"}),
-					frappe.format(used_pi, {"fieldtype": "Currency"}),
-					frappe.format(used_pc, {"fieldtype": "Currency"}),
-					frappe.format(total_committed, {"fieldtype": "Currency"}),
-					frappe.format(po_amount, {"fieldtype": "Currency"}),
-					frappe.format(available_budget, {"fieldtype": "Currency"}),
-					frappe.format(exceeded_amount, {"fieldtype": "Currency"})
-				),
-				title=_("Budget Exceeded")
-			)
+		check_budget_for_item(doc.project, budget_item, po_amount, _("Purchase Order"))
 
 
 def validate_budget_on_material_request_submit(doc, method):
@@ -183,50 +179,7 @@ def validate_budget_on_material_request_submit(doc, method):
 
 	# Validate each project + budget item
 	for (project, budget_item), mr_amount in budget_map.items():
-		budget_info = get_effective_budget_for_item(project, budget_item)
-
-		if not budget_info:
-			frappe.throw(
-				_("No budget found for Budget Expense Item: {0} in Project: {1}. Please create a Budget Expense document first.").format(
-					budget_item, project
-				),
-				title=_("Budget Not Found")
-			)
-
-		total_budget = flt(budget_info["total_budget"])
-		used_po = flt(budget_info["used_po"])
-		used_pi = flt(budget_info["used_pi"])
-		used_pc = flt(budget_info["used_pc"])
-
-		# Budget commitment is PO + PC only; PI is informational (same as Budget Expense)
-		total_committed = get_committed_budget_usage(used_po, used_pc)
-		available_budget = total_budget - total_committed
-
-		# Check if this Material Request would exceed budget
-		if total_committed + mr_amount > total_budget:
-			exceeded_amount = (total_committed + mr_amount) - total_budget
-			frappe.throw(
-				_("Budget exceeded for Budget Expense Item: <b>{0}</b><br><br>"
-				  "Total Budget: {1}<br>"
-				  "Already Used (PO): {2}<br>"
-				  "Already Used (PI): {3}<br>"
-				  "Already Used (PC): {4}<br>"
-				  "Committed (PO + PC): {5}<br>"
-				  "This Material Request Amount: {6}<br>"
-				  "Available Budget: {7}<br>"
-				  "<b>Exceeded by: {8}</b>").format(
-					budget_item,
-					frappe.format(total_budget, {"fieldtype": "Currency"}),
-					frappe.format(used_po, {"fieldtype": "Currency"}),
-					frappe.format(used_pi, {"fieldtype": "Currency"}),
-					frappe.format(used_pc, {"fieldtype": "Currency"}),
-					frappe.format(total_committed, {"fieldtype": "Currency"}),
-					frappe.format(mr_amount, {"fieldtype": "Currency"}),
-					frappe.format(available_budget, {"fieldtype": "Currency"}),
-					frappe.format(exceeded_amount, {"fieldtype": "Currency"})
-				),
-				title=_("Budget Exceeded")
-			)
+		check_budget_for_item(project, budget_item, mr_amount, _("Material Request"))
 
 
 def validate_budget_on_pc_clearance_submit(doc, method):
@@ -257,64 +210,5 @@ def validate_budget_on_pc_clearance_submit(doc, method):
 	
 	# Validate each budget item
 	for budget_item, pc_amount in budget_map.items():
-		# Get budget from submitted Budget Expense documents
-		budget_data = frappe.db.sql("""
-			SELECT
-				bed.budget_item_name,
-				SUM(bed.budget_item_cost) as total_budget,
-				SUM(bed.used_cost_po) as used_po,
-				SUM(bed.used_cost_pi) as used_pi,
-				SUM(bed.used_cost_pc) as used_pc,
-				be.project
-			FROM `tabBudget Expense Detail` bed
-			INNER JOIN `tabBudget Expense` be ON bed.parent = be.name
-			WHERE bed.budget_item_name = %s
-			AND be.project = %s
-			AND be.docstatus = 1
-			GROUP BY bed.budget_item_name, be.project
-		""", (budget_item, doc.project), as_dict=True)
-		
-		if not budget_data:
-			frappe.throw(
-				_("No budget found for Budget Expense Item: {0} in Project: {1}. Please create a Budget Expense document first.").format(
-					budget_item, doc.project
-				),
-				title=_("Budget Not Found")
-			)
-		
-		budget_info = budget_data[0]
-		total_budget = flt(budget_info.total_budget or 0)
-		used_po = flt(budget_info.used_po or 0)
-		used_pi = flt(budget_info.used_pi or 0)
-		used_pc = flt(budget_info.used_pc or 0)
-		
-		# Budget commitment is PO + PC only; PI is informational (same as Budget Expense)
-		total_committed = get_committed_budget_usage(used_po, used_pc)
-		available_budget = total_budget - total_committed
-
-		# Check if this PC Clearance would exceed budget
-		if total_committed + pc_amount > total_budget:
-			exceeded_amount = (total_committed + pc_amount) - total_budget
-			frappe.throw(
-				_("Budget exceeded for Budget Expense Item: <b>{0}</b><br><br>"
-				  "Total Budget: {1}<br>"
-				  "Already Used (PO): {2}<br>"
-				  "Already Used (PI): {3}<br>"
-				  "Already Used (PC): {4}<br>"
-				  "Committed (PO + PC): {5}<br>"
-				  "This PC Clearance Amount: {6}<br>"
-				  "Available Budget: {7}<br>"
-				  "<b>Exceeded by: {8}</b>").format(
-					budget_item,
-					frappe.format(total_budget, {"fieldtype": "Currency"}),
-					frappe.format(used_po, {"fieldtype": "Currency"}),
-					frappe.format(used_pi, {"fieldtype": "Currency"}),
-					frappe.format(used_pc, {"fieldtype": "Currency"}),
-					frappe.format(total_committed, {"fieldtype": "Currency"}),
-					frappe.format(pc_amount, {"fieldtype": "Currency"}),
-					frappe.format(available_budget, {"fieldtype": "Currency"}),
-					frappe.format(exceeded_amount, {"fieldtype": "Currency"})
-				),
-				title=_("Budget Exceeded")
-			)
+		check_budget_for_item(doc.project, budget_item, pc_amount, _("PC Clearance"))
 
